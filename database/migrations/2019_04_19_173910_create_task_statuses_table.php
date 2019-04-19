f7680109d65fcedf1883e8225e3cf2431b9bd136
jclavo <jclavo@jclavo-PC>
2019-04-19 18:13:26 -0500

    Modify tables' name status by statuses

--- database/migrations/2019_04_19_173910_create_task_statuses_table.php
+++ database/migrations/2019_04_19_173910_create_task_statuses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTaskStatusTable extends Migration
+class CreateTaskStatusesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTaskStatusTable extends Migration
      */
     public function up()
     {
-        Schema::create('task_status', function (Blueprint $table) {
+        Schema::create('task_statuses', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->char('description', 45);
             $table->timestamps();
