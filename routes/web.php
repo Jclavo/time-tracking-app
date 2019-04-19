73e66ee1959708d22a1dd9727cc7307ee7b5e7a4
jclavo <jclavo@jclavo-PC>
2019-04-19 15:51:25 -0500

    add routes User and Project

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('users', 'UserController');
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('projects', 'ProjectController');
+
