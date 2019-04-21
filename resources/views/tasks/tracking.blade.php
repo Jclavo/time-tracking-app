1db7e3397852733410b1795006d4bb7ab560f5b8
jclavo <jclavo@jclavo-PC>
2019-04-21 11:05:16 -0500

    Download resources from calendar, so they are local now

--- resources/views/tasks/tracking.blade.php
+++ resources/views/tasks/tracking.blade.php
@@ -4,8 +4,8 @@
 @section('other_styles')
 	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 	
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.1.0/fullcalendar.min.css' />
-    
+	<link href="{{ asset('css/calendar/fullcalendar.min.css') }}" rel="stylesheet">
+	
     <!--Styling for calendar-->
     
     <style>
@@ -93,9 +93,9 @@
 
 @section('other_scripts')
 	
-    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-    <script src='https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.1/moment.min.js'></script>
-    <script src='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.1.0/fullcalendar.min.js'></script>
+    <script src="{{ asset('js/jquery/jquery-3.4.0.min.js') }}"></script>
+    <script src="{{ asset('js/calendar/moment.min.js') }}"></script>
+    <script src="{{ asset('js/calendar/fullcalendar.min.js') }}"></script>
     <script>
         $(document).ready(function() {
             // page is now ready, initialize the calendar...
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,29 @@
 @extends('layouts.app') 
+
 @section('content') 
+
 
 @if (Auth::check())
 
-<div class="container">
-    
-</div>
+    <div class="container">
+        <h3>Project definition</h3>
+        <p>In a small company there is a team which contains developers, project managers and marketing.
+          They need a web based tool to track working hours on very project they have. 
+          In this tracking owner of company wants to know on which project who was working, when,
+          what he was doing, what is the status of task (open, closed, implementation etc . ),
+          what is the percentage of complete. 
+          The tasks are created by project managers and developers only for them. 
+          <b>The purpose of a task is just time tracking not project management.</b></p>
+    </div>
 
 @else
 
-<h3>
-	You need to log in. <a href="{{ route('login') }}">Click here to login</a>
+    <h3>
+    	You need to log in. <a href="{{ route('login') }}">Click here to login</a>
+    
+    </h3>
 
-</h3>
 
-<p>Created by jclavotafur@gmail.com</p>
 
 @endif 
 @endsection
