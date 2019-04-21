63dcbaa960070f112af0fad9566c3c02a8f95264
jclavo <jclavo@jclavo-PC>
2019-04-21 12:32:32 -0500

    Task's option create/store are out the validation

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,16 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+
 Route::get('users', 'UserController@index')->name('users.index');
 Route::get('projects', 'ProjectController@index')->name('projects.index');
 
 Route::match(array('GET','POST'),'tasks/tracking', 'TaskController@tracking')->name('tasks.tracking')->middleware('auth','admin');
 
 Route::get('tasks', 'TaskController@index')->name('tasks.index');
+Route::get('tasks/create', 'TaskController@create')->name('tasks.create');
+Route::post('tasks/store', 'TaskController@store')->name('tasks.store');
+//Route::get('/home', 'HomeController@index')->name('home');
 
 //GROUPS
 Route::group(['middleware' => 'admin'], function() {
@@ -30,11 +34,11 @@ Route::group(['middleware' => 'admin'], function() {
     Route::resource('users', 'UserController', ['except' => 'index']);
     //Route::resource('users', 'UserController');
     Route::resource('projects', 'ProjectController', ['except' => 'index']);
-    Route::resource('tasks', 'TaskController', ['except' => 'index']);
+    Route::resource('tasks', 'TaskController', ['except' => ['index','create','store']]);
     
 });
 
-//Route::get('/home', 'HomeController@index')->name('home');
+
 
 
 
